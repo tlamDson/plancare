550d5671c631eae473c42c00bf8774a7cf965d88
Lam Pham <phamla@dickinson.edu>
2025-12-25 21:44:22 -0500

    Finish set up frontend

---
+++ frontend/mobile/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl font-bold">Welcome to Travel Plan!</Text>
+      <Text className="text-base mt-4">Mobile App with NativeWind</Text>
+    </View>
+  );
+}
